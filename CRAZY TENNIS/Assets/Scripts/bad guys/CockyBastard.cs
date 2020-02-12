db5b1fb0f279ed670ec26b4d97af84ea705897e0
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2020-02-11 21:40:24 -0600

    animate cocky bastard hitting the ball

--- CRAZY TENNIS/Assets/Scripts/bad guys/CockyBastard.cs
+++ CRAZY TENNIS/Assets/Scripts/bad guys/CockyBastard.cs
@@ -8,13 +8,17 @@ public class CockyBastard : BadThing
     private int phase;
     private int rallyCount;
     private PlayerBehaviour pb;
+    private Animator anim;
+    private bool BallSpawnTriggered;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         phase = 0;
         NextPhase();
         pb = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
         pb.Breakout = true;
+        BallSpawnTriggered = false;
     }
 
     // Update is called once per frame
@@ -25,9 +29,10 @@ public class CockyBastard : BadThing
             Destroy(ball.gameObject);
             ball = null;
         }
-        if(ball == null)
+        if(ball == null && !BallSpawnTriggered)
         {
-            ball = SpawnBallPhase(phase);
+            anim.SetTrigger("swing");
+            BallSpawnTriggered = true;
             rallyCount = phase - 1;
         }
     }
@@ -49,9 +54,9 @@ public class CockyBastard : BadThing
         if(phase == 1)
         {
             return SpawnBall(
-                typeof(GenericHittable), 
-                transform.position, 
-                new Vector2(0, -3), 
+                typeof(GenericHittable),
+                transform.position + new Vector3(-0.7f, -0.2f, 0),
+                new Vector2(0, -3),
                 Random.Range(6f, 10f)
             );
         }
@@ -59,7 +64,7 @@ public class CockyBastard : BadThing
         {
             return SpawnBall(
                 typeof(GenericHittable), 
-                transform.position, 
+                transform.position + new Vector3(-0.7f, -0.2f, 0),
                 new Vector2(Random.Range(-3f, 3f), Random.Range(-3f, -4f)),
                 Random.Range(6f, 10f)
             );
@@ -68,8 +73,8 @@ public class CockyBastard : BadThing
         {
             return SpawnBall(
                 typeof(GenericHittable), 
-                transform.position, 
-                (pb.transform.position - transform.position).normalized * 5, 
+                transform.position + new Vector3(-0.7f, -0.2f, 0),
+                (pb.transform.position - transform.position).normalized * 5,
                 Random.Range(6f, 10f)
             );
         }
@@ -83,15 +88,27 @@ public class CockyBastard : BadThing
             base.Ouch(ball);
         else
         {
+            anim.SetTrigger("swing");
             print("rally " + rallyCount);
             rallyCount--;
             ball.hit = false;
             Physics2D.IgnoreCollision(ball.GetComponent<Collider2D>(), this.GetComponent<Collider2D>(), true);
+        }
+    }
+
+    public void HitTime()
+    {
+        if(ball == null && BallSpawnTriggered)
+        {
+            ball = SpawnBallPhase(phase);
+            BallSpawnTriggered = false;
+        }
+        else
+        {
             if(phase == 2)
                 ball.Velocity = new Vector2(Random.Range(-3f, 3f), Random.Range(-3f, -4f));
             else if(phase == 3)
                 ball.Velocity = (pb.transform.position - transform.position).normalized * 5;
         }
     }
-
 }
