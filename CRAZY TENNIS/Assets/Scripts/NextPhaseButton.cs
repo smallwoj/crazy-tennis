d0fec18ef7db5b36f536d0d2f6a68809268f5d98
drkitt <alex.kitt@outlook.com>
2020-05-03 22:07:15 -0500

    Added skip-to-next-phase button. Hopefully we remember to remove this before release!

---
+++ CRAZY TENNIS/Assets/Scripts/NextPhaseButton.cs
@@ -0,0 +1,16 @@
+﻿// Code for an on-screen button that automatically advances to the current enemy's next phase.
+// Used for ease of debugging specific enemeies
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextPhaseButton : MonoBehaviour
+{
+    // ButtonPress is called whenever the button is pressed
+    public void ButtonPress()
+    {
+        // One-liner time baybee! It's necessary to load the enemy each time, in order to account for NextEnemy calls
+        GameObject.FindGameObjectWithTag("Enemy").GetComponent<BadThing>().NextPhase();
+    }
+}
