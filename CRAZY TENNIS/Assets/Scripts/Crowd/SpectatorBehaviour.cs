7d570abbb7084be1d24a69d0bad0c8559984518b
drkitt <alex.kitt@outlook.com>
2020-05-07 18:12:16 -0500

    The crowd cheers! It's very janky, though; I gotta refine a bunch of constants' values

--- CRAZY TENNIS/Assets/Scripts/Crowd/SpectatorBehaviour.cs
+++ CRAZY TENNIS/Assets/Scripts/Crowd/SpectatorBehaviour.cs
@@ -6,6 +6,23 @@ using UnityEngine;
 
 public class SpectatorBehaviour : MonoBehaviour
 {
+    /// <summary> How many times the spectator may jump during a cheer </summary>
+    private static readonly float MAX_JUMPS = 7;
+    /// <summary> How many (world space?) distance units high the spectator can jump </summary>
+    private static readonly float MAX_HEIGHT = 1;
+    /// <summary> How many times the spectator can jump per second </summary>
+    private static readonly float MAX_SPEED = 10;
+    /// <summary> Whether the spectator is currently cheering </summary>
+    private bool hyped = false;
+    /// <summary> How many seconds are left in the cheer </summary>
+    private float timeLeft = 0;
+    /// <summary> How many (world space?) distance units high the spectator jumps </summary>
+    private float jumpHeight = 0;
+    /// <summary> How many times the spectator jumps per second </summary>
+    private float speed = 1;
+    /// <summary> The spectator's position before they did the whole cheering thing </summary>
+    private Vector3 originalPos = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +32,21 @@ public class SpectatorBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (hyped)
+        {
+            transform.position = new Vector3(
+                originalPos.x, 
+                originalPos.y + jumpHeight * Mathf.Abs(Mathf.Sin(Mathf.PI * speed * timeLeft)), 
+                originalPos.z);
+
+            timeLeft -= Time.deltaTime;
+
+            if (timeLeft <= 0)
+            {
+                hyped = false;
+                transform.position = originalPos;
+            }
+        }
     }
 
     /// <summary>
@@ -26,6 +57,20 @@ public class SpectatorBehaviour : MonoBehaviour
     /// and it plays a (as of yet undetermined) role in deciding the intensity and length of a spectator's cheer</param>
     public void Cheer(float hype)
     {
-        print(hype);
+
+        // Randomly decide whether or not to cheer, based on the hype. A hype of 1 guarantees a cheer
+        if (!hyped && Random.value <= hype)
+        {
+            hyped = true;
+
+            // Randomly decide the jump's properties
+            jumpHeight = Random.value * MAX_HEIGHT;
+            speed = Random.value * MAX_SPEED;
+            // The calcuation for the duration of the cheer ensures that the spectator will start their cheer on the ground 
+            timeLeft = (int)(Random.value * MAX_JUMPS) / speed;
+
+            // Record the position before cheering
+            originalPos = transform.position;
+        }
     }
 }
