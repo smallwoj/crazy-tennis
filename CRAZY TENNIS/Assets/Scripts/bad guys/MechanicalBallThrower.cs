0d7bec5c6ccd433deac6ce13470f1e68c6156a3b
drkitt <alex.kitt@outlook.com>
2020-05-16 21:12:12 -0500

    Mechanical ball thrower complete? Not to jinx it but that was way quicker than I expected :D

--- CRAZY TENNIS/Assets/Scripts/bad guys/MechanicalBallThrower.cs
+++ CRAZY TENNIS/Assets/Scripts/bad guys/MechanicalBallThrower.cs
@@ -7,21 +7,25 @@ using UnityEngine;
 public class MechanicalBallThrower : BadThing
 {
     // Constants
+    /// <summary> The displacement between the enemy's position and the ball's initial position </summary>
+    private static readonly Vector3 BALL_OFFSET = new Vector3(0, -0.35f, 0);
     /// <summary> How long (in seconds) it takes to go from one charging animation to the other. Indexed by phase  </summary>
-    private static readonly float[] CHARGE_ANIM_TIMES = {0, 1/8, 1, 3};
+    private static readonly float[] CHARGE_ANIM_TIMES = {0, 1/8, 1, 2};
     /// <summary> How fast the ball goes in each phase </summary>
-    private static readonly float[] BALL_SPEEDS = {0, 7, 15, 5};
+    private static readonly float[] BALL_SPEEDS = {0, 7, 15, 4};
     private static readonly int[] MAX_HITS = {0, 2, 1, 15};
-    /// <summary> The probability that a newly-spawned ball will be unhittable </summary>
-    private static readonly float UNHITTABLE_PROBABILITY = 0.5f;
+    /// <summary> How many balls are sent out during the last phase </summary>
+    private static readonly int NUM_BALLS = 25;
 
     // Instance variables
     /// <summary> Reference to the ball that this thing throws </summary>
     private Ball ball;
+    /// <summary> References to the many balls that this thing throws during its third phase </summary>
+    private Ball[] lastPhaseBalls = new Ball[NUM_BALLS];
     /// <summary>
     /// Current phase
-    /// 1 -> Counts down quickly and then shoots a ball 
-    /// 2 -> Counts down slowly and then shoots a ball at speed 30
+    /// 1 -> Counts down (extremely) quickly and then shoots a ball 
+    /// 2 -> Counts down slowly and then shoots a ball at high speed
     /// 3 -> Maximum Overdrive!
     /// </summary>
     private int phase = 0;
@@ -65,9 +69,9 @@ public class MechanicalBallThrower : BadThing
     {
         DestroyAllBalls();
         phase++;
-        maxhits = MAX_HITS[phase];
         if (phase == 4)
         {
+            DestroyAllBalls();
             SpawnNextEnemy("redCharacter");
         }
         else
@@ -77,9 +81,13 @@ public class MechanicalBallThrower : BadThing
             {
                 base.NextPhase();
             }
+
+            // Get ready for the next phase
+            maxhits = MAX_HITS[phase];
+            chargeTimeRemaining = CHARGE_ANIM_TIMES[phase];
+            anim.SetTrigger("Reset charge");
         }
 
-        chargeTimeRemaining = CHARGE_ANIM_TIMES[phase];
     }
 
     /// <summary>
@@ -87,30 +95,50 @@ public class MechanicalBallThrower : BadThing
     /// </summary>
     public void Fire()
     {
-        // Randomly decide which type of ball to fire
-        // System.Type ballType;
-        // 
-        // if (Random.value < UNHITTABLE_PROBABILITY)
-        // {
-        //     ballType = typeof(GenericUnhittable);
-        // }
-        // else
-        // {
-        //     ballType = typeof(GenericHittable);
-        // }
-
         // Destroy an existing ball to prevent memory leaks
-        if (ball != null)
+        if (ball != null && ball.OutsideCourt)
         {
             Destroy(ball.gameObject);
             ball = null;
         }
 
-        ball = SpawnBall(
-            typeof(GenericHittable),
-            transform.position + new Vector3(0, -0.35f, 0),
-            new Vector2(0, -BALL_SPEEDS[phase]),
-            Random.Range(6f, 10f)
-            );
+        // Based on the phase, fire either one ball or quite a few balls
+        if (phase < 3)
+        {
+            // Fire one ball
+            ball = SpawnBall(
+                typeof(GenericHittable),
+                transform.position + BALL_OFFSET,
+                new Vector2(0, -BALL_SPEEDS[phase]),
+                Random.Range(6f, 10f)
+                );
+        }
+        else
+        {
+            // Fire several balls. This is like TestBadGuy's entire gimmick, 
+            // except the balls are only fired once
+            for (int i = 0; i < NUM_BALLS; i++)
+            {
+                // Clean up this ball from the previous call to this method
+                if (lastPhaseBalls[i] != null && lastPhaseBalls[i].OutsideCourt)
+                {
+                    GameObject.Destroy(lastPhaseBalls[i]);
+                    lastPhaseBalls[i] = null;
+                }
+
+                // Prepare a velocity vector that uses a random angle
+                Vector2 velocity = new Vector2(Random.Range(-1/2f, 1/2f), Random.Range(-1f, -1/2f));
+                velocity.Normalize();
+                velocity *= Random.value * BALL_SPEEDS[phase];
+
+                // Fire this ball at a random angle
+                lastPhaseBalls[i] = SpawnBall(
+                    typeof(GenericHittable), 
+                    transform.position + BALL_OFFSET,
+                    velocity,
+                    velocity.magnitude * 2
+                    );
+            }
+        }
     }
 }
