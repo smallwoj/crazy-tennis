57260c92af20be530490fa839b0e316f72113b40
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2020-05-20 01:56:20 -0500

    Dog now dynamically animates based on direection
    no down animations? wack
    it looks really weird because of collision between the net? wack

--- CRAZY TENNIS/Assets/Scripts/bad guys/good boy/Dog.cs
+++ CRAZY TENNIS/Assets/Scripts/bad guys/good boy/Dog.cs
@@ -1,18 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Dog : GoodBoy
 {
+    private Vector2 to, from;
+    private float t;
+    private float inc;
+    private int phase;
+    private Ball ball;
+    private Rigidbody2D rb;
+    private Vector2 prev;
+    private Animator anim;
+    new private SpriteRenderer renderer;
     // Start is called before the first frame update
     new void Start()
     {
-        
+        base.Start();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        renderer = GetComponent<SpriteRenderer>();
+        phase = 0;
+        NextPhase();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        prev = rb.position;
+        if (t < 1)
+        {
+            inc = 0.05f / Vector2.Distance(to, from);
+            t += inc;
+            if(t >= 1)
+                t = 1f;
+            float T = (float)(1f - System.Math.Cos(t * System.Math.PI))/2f;
+            rb.position = Vector2.Lerp(from, to, T);
+        }
+        DetermineDirection();
+    }
+
+    private void DetermineDirection()
+    {
+        List<string> names = new List<string>(new string[] {"up", "right", "down", "left"});
+        foreach (string name in names)
+            anim.SetBool(name, false);
+        Vector2 dir = (rb.position - prev).normalized;
+        if(dir.magnitude != 0)
+        {
+            List<Vector2> directions = new List<Vector2>(new Vector2[] {Vector2.up, Vector2.right, Vector2.down, Vector2.left});
+            List<float> dots = directions.Select(x => Vector2.Dot(dir, x)).ToList();
+            string name = names[dots.IndexOf(dots.Max())];
+            anim.SetBool(name, true);
+            renderer.flipX = name == "right";
+        }
+    }
+
+    public override void NextPhase()
+    {
+        phase++;
+        switch (phase)
+        {
+            case 1: // dogy bring ball to you!
+                from = rb.position;
+                to = new Vector2(-0.16f, -1.36f);
+                t = 0;
+            break;
+        }
     }
 }
