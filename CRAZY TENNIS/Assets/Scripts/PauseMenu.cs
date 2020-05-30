96786986d3313aedcb85b78be0b438e1d7fdaeb2
drkitt <alex.kitt@outlook.com>
2020-05-30 14:16:13 -0500

    Refactored the ball rotation code slightly to pause along with the rest of the game. Also wrote a fancier pausing algorithm and then commented it out

--- CRAZY TENNIS/Assets/Scripts/PauseMenu.cs
+++ CRAZY TENNIS/Assets/Scripts/PauseMenu.cs
@@ -24,13 +24,59 @@ public class PauseMenu : MonoBehaviour
         {
             if (paused)
             {
-                Time.timeScale = 0;
+                Time.timeScale = 1;
             }
             else
             {
-                Time.timeScale = 1;
+                Time.timeScale = 0;
             }
             paused = !paused;
+
+            // This block is part of the more complicated pausing algorithm, 
+            // ranted about below
+            /*
+            if (!pausing)
+            {
+                pausing = true;
+            }
+            */
         }
+
+        /* More complicated pausing algorithm that visibly slows down time 
+        instead of instantly stopping gameplay. 
+        It's commented out because if we go with this, I'd want it to slow down 
+        the music and sound effects too, but it's not possible to test that 
+        kinda thing right now, without music nor sound effects :P
+
+        All variables are private instance variables, with timeAcceleration 
+        having default value -0.025 and all the bools having default value false
+        */
+        /*
+        if (pausing)
+        {
+            // Get the new time scale
+            newTimeScale = Time.timeScale + timeAcceleration;
+
+            // Once time is either stopped or returned to normal, reset 
+            // variables
+            if (newTimeScale < 0 || newTimeScale > 1)
+            {
+                pausing = !pausing;
+                timeAcceleration = -timeAcceleration;
+
+                if (newTimeScale < 0)
+                {
+                    newTimeScale = 0;
+                }
+                else    // newTimeScale > 1
+                {
+                    newTimeScale = 1;
+                }
+            }
+
+            // Gradually slow down time
+            Time.timeScale = newTimeScale;
+        }
+        */
     }
 }
