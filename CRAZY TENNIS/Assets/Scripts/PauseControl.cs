d0c98e60d9ce0d62848f9d970f027167554d4948
drkitt <alex.kitt@outlook.com>
2020-05-30 14:49:51 -0500

    Separated the pausing script from the pause menu. This will enable more convenient control over the entire pause menu. Oh, and the screen dims now!

---
+++ CRAZY TENNIS/Assets/Scripts/PauseControl.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Pauses the game when the player presses a specific button
+/// </summary>
+public class PauseControl : MonoBehaviour
+{
+    /// <summary> Whether the game is currently paused </summary>
+    private bool paused = false;
+
+    /// <summary> The pause menu that this script enables </summary>
+    private GameObject menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Menu! Where are you???
+        menu = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Pause and unpause when the cancel button (right now, Esc) is pressed
+        if (Input.GetButtonDown("Cancel"))
+        {
+            paused = !paused;
+            menu.SetActive(paused);
+            // If this were C(++) I'd be able to say Time.timeScale = paused 😤
+            if (paused)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+
+            // This block is part of the more complicated pausing algorithm, 
+            // ranted about below
+            /*
+            if (!pausing)
+            {
+                pausing = true;
+            }
+            */
+        }
+
+        /* More complicated pausing algorithm that visibly slows down time 
+        instead of instantly stopping gameplay. 
+        It's commented out because if we go with this, I'd want it to slow down 
+        the music and sound effects too, but it's not possible to test that 
+        kinda thing right now, without music nor sound effects :P
+
+        All variables are private instance variables, with timeAcceleration 
+        having default value -0.025 and all the bools having default value false
+        */
+        /*
+        if (pausing)
+        {
+            // Get the new time scale
+            newTimeScale = Time.timeScale + timeAcceleration;
+
+            // Once time is either stopped or returned to normal, reset 
+            // variables
+            if (newTimeScale < 0 || newTimeScale > 1)
+            {
+                pausing = !pausing;
+                timeAcceleration = -timeAcceleration;
+
+                if (newTimeScale < 0)
+                {
+                    newTimeScale = 0;
+                }
+                else    // newTimeScale > 1
+                {
+                    newTimeScale = 1;
+                }
+            }
+
+            // Gradually slow down time
+            Time.timeScale = newTimeScale;
+        }
+        */
+    }
+}
--- CRAZY TENNIS/Assets/Scripts/PauseMenu.cs
+++ CRAZY TENNIS/Assets/Scripts/PauseMenu.cs
@@ -2,14 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Pauses the game when the player presses a specific button
-/// </summary>
 public class PauseMenu : MonoBehaviour
 {
-    /// <summary> Whether the game is currently paused </summary>
-    private bool paused = false;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -19,64 +13,13 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Pause and unpause when the cancel button (right now, Esc) is pressed
-        if (Input.GetButtonDown("Cancel"))
-        {
-            if (paused)
-            {
-                Time.timeScale = 1;
-            }
-            else
-            {
-                Time.timeScale = 0;
-            }
-            paused = !paused;
+        
+    }
 
-            // This block is part of the more complicated pausing algorithm, 
-            // ranted about below
-            /*
-            if (!pausing)
-            {
-                pausing = true;
-            }
-            */
-        }
-
-        /* More complicated pausing algorithm that visibly slows down time 
-        instead of instantly stopping gameplay. 
-        It's commented out because if we go with this, I'd want it to slow down 
-        the music and sound effects too, but it's not possible to test that 
-        kinda thing right now, without music nor sound effects :P
-
-        All variables are private instance variables, with timeAcceleration 
-        having default value -0.025 and all the bools having default value false
-        */
-        /*
-        if (pausing)
-        {
-            // Get the new time scale
-            newTimeScale = Time.timeScale + timeAcceleration;
-
-            // Once time is either stopped or returned to normal, reset 
-            // variables
-            if (newTimeScale < 0 || newTimeScale > 1)
-            {
-                pausing = !pausing;
-                timeAcceleration = -timeAcceleration;
-
-                if (newTimeScale < 0)
-                {
-                    newTimeScale = 0;
-                }
-                else    // newTimeScale > 1
-                {
-                    newTimeScale = 1;
-                }
-            }
-
-            // Gradually slow down time
-            Time.timeScale = newTimeScale;
-        }
-        */
+    /// <summary>
+    /// Called when the game is paused
+    /// </summary>
+    private void OnEnable() {
+        print("win");
     }
 }
