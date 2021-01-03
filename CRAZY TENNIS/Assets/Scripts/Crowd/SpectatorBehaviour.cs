bda6a8a504a29f2c760ab5953a6387ba1493655f
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2021-01-03 15:40:18 -0600

    Remove single line of code thabk

--- CRAZY TENNIS/Assets/Scripts/Crowd/SpectatorBehaviour.cs
+++ CRAZY TENNIS/Assets/Scripts/Crowd/SpectatorBehaviour.cs
@@ -3,7 +3,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 
 public class SpectatorBehaviour : MonoBehaviour, Spectator
 {
